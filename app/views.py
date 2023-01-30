067d318227c0a63072ba4c549d001a21c3321c90
crispindeity <shoy1415@gmail.com>
2023-01-30 22:36:00 +0900

    :sparkles: AOP 를 활용한 로그인 구현

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import CreateView
 
 from app.forms import PostForm
 from app.models import Post
 
 
-# Create your views here.
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
+    # if not request.user.is_authenticated:
+    #     return redirect("/accounts/login/")
     qs = Post.objects.all()
     return render(request, "app/index.html", {"post_list": qs})
 
